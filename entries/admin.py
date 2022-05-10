401df433925752bf76b31afe34424b6f67b2a9e1
Zhi Shao <jhintr@gmail.com>
2022-05-10 19:46:05 +0800

    create Entry model

--- entries/admin.py
+++ entries/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.contrib.auth.models import Group, User
+
+from .models import Entry
 
 
-# Register your models here.
+class EntryAdmin(ModelAdmin):
+    """Entry Admin"""
+
+    list_display = ("pali", "grammar")
+    search_fields = ("pali", "roman")
+
+
+admin.site.register(Entry, EntryAdmin)
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
--- entries/models.py
+++ entries/models.py
@@ -1,4 +1,48 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 
-# Create your models here.
+WORD_LENGTH = 20
+
+ROMANIZE = {
+    "ā": "a",
+    "ī": "i",
+    "ū": "u",
+    "ṃ": "m",
+    "ṅ": "n",
+    "ñ": "n",
+    "ṭ": "t",
+    "ḍ": "d",
+    "ṇ": "n",
+    "ḷ": "l",
+}
+
+
+class Entry(models.Model):
+    """Pali Entry"""
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    pali = models.CharField(max_length=WORD_LENGTH)
+    roman = models.CharField(max_length=WORD_LENGTH, null=True, blank=True)
+    sanskrit = models.CharField(max_length=WORD_LENGTH, null=True, blank=True)
+    grammar = models.CharField(max_length=WORD_LENGTH, null=True, blank=True)
+    content = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["pali"]
+        verbose_name_plural = "Entries"
+
+    def __str__(self) -> str:
+        return self.pali
+
+
+@receiver(pre_save, sender=Entry)
+def pre_save_entry_latin(instance, **kwargs):
+    """Generate Entry's roman"""
+
+    pali = instance.pali
+    roman = [ROMANIZE[c] if c in ROMANIZE else c for c in pali]
+    instance.roman = "".join(roman)
